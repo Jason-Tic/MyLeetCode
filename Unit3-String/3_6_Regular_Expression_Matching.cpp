54d56b350990c18e0d9ae4bc6317ce899d74da66
= <=>
2022-02-15 22:46:34 +0800

    2022-02-15

---
+++ Unit3-String/3_6_Regular_Expression_Matching.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+using namespace std;
+class Solution{
+public:
+    bool isMatch(const char *s, const char *p)
+    {
+        if(*p == '\0') return *s == '\0';
+    }
+};
+int main()
+{
+    
+    return 0;
+}
