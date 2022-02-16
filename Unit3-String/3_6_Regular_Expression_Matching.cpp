84ec4466467a1cd2f730455069ecd9a6bc567d34
TT PSW <1210508213@qq.com>
2022-02-16 16:56:01 +0800

    2022-02-16

--- Unit3-String/3_6_Regular_Expression_Matching.cpp
+++ Unit3-String/3_6_Regular_Expression_Matching.cpp
@@ -2,15 +2,42 @@
 #include <cstdio>
 #include <string>
 using namespace std;
-class Solution{
+class Solution
+{
 public:
     bool isMatch(const char *s, const char *p)
     {
-        if(*p == '\0') return *s == '\0';
+        cout << "*s=" << *s << ";*p=" << *p << endl;
+        if (*p == '\0')
+            return *s == '\0';
+
+        // next char is not '*', then must match current character
+        if (*(p + 1) != '*')
+        {
+            // cout << *(p + 1) << endl;
+            if (*p == *s || (*p == '.' && *s != '\0'))
+            {
+                // cout<<s+1<<"  "<<p+1<<endl;
+                return isMatch(s + 1, p + 1);
+            }
+            else
+                return false;
+        }
+        else // next char is '*'
+        {
+            while (*p == *s || (*p == '.' && *s != '\0'))
+            {
+                if (isMatch(s, p + 2))
+                    return true;
+                s++;
+            }
+            return isMatch(s, p + 2);
+        }
     }
 };
 int main()
 {
-    
+    Solution S1;
+    cout << S1.isMatch("aab", "c*a*b") << endl;
     return 0;
 }
--- Unit3-String/3_7_Wildcard_Matching.cpp
+++ Unit3-String/3_7_Wildcard_Matching.cpp
@@ -2,11 +2,31 @@
 #include <cstdio>
 #include <string>
 using namespace std;
-class Solution{
+class Solution
+{
 public:
+    bool isMatch(const char *s, const char *p)
+    {
+        if (*p == '*')
+        {
+            while (*p == '*')
+                ++p;
+            if (*p == '\0')
+                return true;
+            while (*s != '\0' && isMatch(s, p))
+                ++s;
+            return *s != '\0';
+        }
+        else if (*p == '\0' || *s == '\0')
+            return *p == *s;
+        else if (*p == *s || *p == '?')
+            return isMatch(++s, ++p);
+        else
+            return false;
+    }
 };
 int main()
 {
-    
+
     return 0;
 }
