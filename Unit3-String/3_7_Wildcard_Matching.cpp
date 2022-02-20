8b75a04a258a2e16c361e89b30bb1b0c599715b3
= <=>
2022-02-20 21:44:27 +0800

    2022-02-20

--- Unit3-String/3_7_Wildcard_Matching.cpp
+++ Unit3-String/3_7_Wildcard_Matching.cpp
@@ -24,9 +24,47 @@ public:
         else
             return false;
     }
+
+    bool isMatch2(const char *s, const char *p)
+    {
+        bool star = false; //是否是*的标记
+        const char *str = s;
+        const char *ptr = p;
+        for (; *str != '\0'; str++, ptr++)
+        {
+            switch (*ptr)
+            {
+            case '?': /* constant-expression */
+                /* code */
+                break;
+            case '*':
+                star = true;
+                s = str, p = ptr;
+                while (*p == '*')
+                    p++; // skip continuous '*'
+                if (*p == '\0')
+                    return true;
+                str = s - 1;
+                ptr = p - 1;
+                break;
+            default:
+                if (*str != *ptr)
+                {
+                    if (!star)
+                        return false;
+                    str = s - 1;
+                    ptr = s - 1;
+                }
+            }
+        }
+        while (*p == '*')
+            ptr++;
+        return (*ptr == '\0');
+    }
 };
 int main()
 {
-
+    Solution s1;
+    cout << s1.isMatch2("aab", "a*b") << endl;
     return 0;
 }
--- Unit3-String/3_8_Longest_Common_Prefix.cpp
+++ Unit3-String/3_8_Longest_Common_Prefix.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <cstdio>
 #include <string>
+#include <vector>
 using namespace std;
 class Solution{
 public:
+    string longestCommonPrefix(vector<string> &strs)
+    {
+        if(strs.empty()) return "";
+        for(int idx=0;idx<strs.size();idx++){
+            for(int i=1;i<strs.size();i++){
+                if(strs[i][idx]!=strs[0][idx]) return strs[0].substr(0,idx);
+            }
+        }
+    } 
 };
 int main()
 {
