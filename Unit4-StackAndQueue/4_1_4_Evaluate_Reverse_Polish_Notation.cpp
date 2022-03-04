d46568e77b16746c8924c9b1549355547f070035
TT PSW <1210508213@qq.com>
2022-03-04 13:59:11 +0800

    2022-03-04

--- Unit4-StackAndQueue/4_1_4_Evaluate_Reverse_Polish_Notation.cpp
+++ Unit4-StackAndQueue/4_1_4_Evaluate_Reverse_Polish_Notation.cpp
@@ -6,6 +6,11 @@ using namespace std;
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        
+        cout<<this->is_poerator("+")<<endl;
+    }
+private:
+    bool is_poerator(const string &op)
+    {
+        return op.size() == 1 && string("+-*/").find(op) != string::npos;
     }
 };
