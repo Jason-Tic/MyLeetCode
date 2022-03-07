febdea886e8bee1ae944a525ee5950490b5cd90d
TT PSW <1210508213@qq.com>
2022-03-07 16:55:10 +0800

    2022-03-07

--- Unit4-StackAndQueue/4_1_4_Evaluate_Reverse_Polish_Notation.cpp
+++ Unit4-StackAndQueue/4_1_4_Evaluate_Reverse_Polish_Notation.cpp
@@ -1,16 +1,50 @@
-#include<iostream>
-#include<vector>
-
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int evalRPN(vector<string>& tokens) {
-        cout<<this->is_poerator("+")<<endl;
+    int evalRPN(vector<string> &tokens)
+    {
+        stack<string> stk;
+        for (auto s : tokens)
+        {
+            if (!is_poerator(s))
+                stk.push(s);
+            else
+            {
+                int y = stoi(stk.top());
+                stk.pop();
+                int x = stoi(stk.top());
+                stk.pop();
+                if (s == "+")
+                    x += y;
+                else if (s == "-")
+                    x -= y;
+                else if (s == "*")
+                    x *= y;
+                else
+                    x /= y;
+                stk.push(to_string(x));
+            }
+        }
+        return stoi(stk.top());
     }
+
 private:
     bool is_poerator(const string &op)
     {
         return op.size() == 1 && string("+-*/").find(op) != string::npos;
     }
 };
+
+int main()
+{
+    vector<string> strings = {"4", "13", "5", "/", "+"};
+    Solution s1;
+    cout << s1.evalRPN(strings) << endl;
+    return 0;
+}
