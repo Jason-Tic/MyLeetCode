6d40d71411a7685c3a254f66818734255c87528f
TT PSW <1210508213@qq.com>
2022-03-10 16:56:16 +0800

    2022-03-10

--- Unit5-Tree/5-1/5_1_1_Binary_Tree_Preorder_Traversal.cpp
+++ Unit5-Tree/5-1/5_1_1_Binary_Tree_Preorder_Traversal.cpp
@@ -3,39 +3,63 @@
 #include <stack>
 using namespace std;
 
-struct TreeNode {
-int val;
+struct TreeNode
+{
+    int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) { }
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 };
 
-class Solution 
+class Solution
 {
 
 public:
-    TreeNode* ReadTree()
+    TreeNode *ReadTree()
     {
-        
     }
 
     vector<int> preorderTraversal(TreeNode *root)
     {
-        vector<int> vec ;
-        stack<const TreeNode *> s; 
-        if(root) s.push(root);
+        vector<int> vec;
+        stack<const TreeNode *> s;
+        if (root)
+            s.push(root);
 
-        while(!s.empty())
+        while (!s.empty())
         {
             auto node = s.top();
             s.pop();
             vec.push_back(node->val);
-            if(node->right) 
+            if (node->right)
                 s.push(node->right);
-            if(node->left)
-                s.push(node->left);                            
+            if (node->left)
+                s.push(node->left);
         }
         return vec;
+    }
+
+    vector<int> inorderTraversal(TreeNode *root)
+    {
+        vector<int> result;
+        stack<const TreeNode *> S;
+        const TreeNode * P = root;
+        while (!S.empty() || P != nullptr)
+        {
+            if (P != nullptr)
+            {
+                S.push(P);
+                P = P->left;
+            }
+            else
+            {
+                P = S.top();
+                S.pop();
+                result.push_back(P->val);
+                P = P->right;
+            }
+        }
+        return result;
     }
 };
 
